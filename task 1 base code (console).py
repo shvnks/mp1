74b7dcaa7098a6325258dd1196484fbeb7a3db1f
Hunter <crypto.hunter@tutanota.com>
2021-10-15 00:27:00 -0400

    sorting + updating

--- task 1 base code (console).py
+++ task 1 base code (console).py
@@ -19,15 +19,15 @@ labels_str = np.array(files.target_names)[labels]
 print(dict(zip(labels_str, counts)))    # creating dictionary
 
 # task 1 step 5 splitting dataset
-x_train, x_test, y_train, y_test = train_test_split(files.data, files.target, train_size=0.8, test_size=0.2)
-print(len(x_train), len(x_test))
+X_train, X_test, y_train, y_test = train_test_split(files.data, files.target, train_size=0.8, test_size=0.2)
+print(len(X_train), len(X_test))
 
 vectorizer = TfidfVectorizer(stop_words="english", max_features=2000) #max_features can be changed
-vectorizer.fit(x_train)
+vectorizer.fit(X_train)
 
 # task 1 step 6
 bayes = MultinomialNB()
-bayes.fit(vectorizer.transform(x_train), y_train)
-y_predict = bayes.predict(vectorizer.transform(x_test))
+bayes.fit(vectorizer.transform(X_train), y_train)
+y_predict = bayes.predict(vectorizer.transform(X_test))
 print(accuracy_score(y_test, y_predict))
 print(classification_report(y_test, y_predict))
--- task2.py
+++ task2.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.neural_network import MLPClassifier
+# from sklearn.datasets import make_classification
 
 # TASK 2 PART 2: Load the Drug.csv in Python by using panda.read_csv.
 df = pd.read_csv("drug200.csv")
@@ -8,7 +14,7 @@ df = pd.read_csv("drug200.csv")
 
 # TASK 2 PART 3: Plot the distribution of the instances in each class and store the graphic in a file called drug-distribution.pdf.
 df.groupby("Drug")["Na_to_K"].nunique().plot(kind="bar")
-plt.savefig("drug-distribution.pdf")
+plt.savefig("output/task2/drug-distribution.pdf")
 
 # TASK 2 PART 4: Convert all ordinal and nominal features in numerical format
 # sexe = pd.get_dummies(df, columns=["Sex"], drop_first=True)
@@ -40,4 +46,29 @@ df.Drug = df.Drug.cat.codes
 # print(df)
 
 # TASK 2 PART 5: Split the dataset using train_test_split using the default parameter values.
-train_test_split(df)
+# print(train_test_split(df))
+# X_train, X_test = train_test_split(df)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+#
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
+# TASK 2 PART 6a: NB: a Gaussian Naive Bayes Classier (naive bayes.GaussianNB) with the default parameters.
+gauss = GaussianNB()
+gauss.fit(X_train)
+print(gauss)
+
+# TASK 2 PART 6b:
+
+# TASK 2 PART 6c:
+
+# TASK 2 PART 6d:
+
+# TASK 2 PART 6e:
+
+# TASK 2 PART 6f:
+
+# TASK 2 PART 7: For each of the 6 classier above, append the following information in ale called drugs-performance.txt:(to make it easier for the TAs, make sure that your output for each sub-question below is clearly marked in your output le, using the headings (a), (b) . . . )
+
+# TASK 2 PART 7a: a clear separator (a sequence of hyphens or stars) and a string clearly describing the model (e.g. the model name + hyper-parameter values that you changed). In the case of Top-DT and Top-MLP, display the best hyperparameters found by the gridsearch.
